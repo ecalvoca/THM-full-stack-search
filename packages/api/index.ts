5a5eebfb7e794f4fd940ee0aa3bfb50f65219e79
ecalvoca <elena.calvo.carazo@gmail.com>
2025-01-28 16:17:18 +0100

    adjust index.ts to use the database service and the routers

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,38 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
-import { MongoClient } from "mongodb";
+import { connect } from './services/database.service';
+import { hotelsRouter } from "./routes/hotels.router";
+import { citiesRouter } from "./routes/cities.router";
+import { countriesRouter } from "./routes/countries.router";
 
 dotenv.config();
 
 if (process.env.NODE_ENV !== 'production' && !process.env.DATABASE_URL) {
-  await import('./db/startAndSeedMemoryDB');
+    await import('./db/startAndSeedMemoryDB');
 }
 
 const PORT = process.env.PORT || 3001;
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/hotels', async (req, res) => {
-  const mongoClient = new MongoClient(DATABASE_URL);
-  console.log('Connecting to MongoDB...');
+connect().then(() => {
+    app.use("/hotels", hotelsRouter);
+    app.use("/countries", countriesRouter);
+    app.use("/cities", citiesRouter);
 
-  try {
-    await mongoClient.connect();
-    console.log('Successfully connected to MongoDB!');
-    const db = mongoClient.db()
-    const collection = db.collection('hotels');
-    res.send(await collection.find().toArray())
-  } finally {
-    await mongoClient.close();
-  }
-})
+    app.listen(PORT, () => {
+      console.log(`API Server Started at ${PORT}`)
+    })
+}).catch((error: Error) => {
+    console.error("Database connection failed", error);
+});
 
-app.listen(PORT, () => {
-  console.log(`API Server Started at ${PORT}`)
-})
