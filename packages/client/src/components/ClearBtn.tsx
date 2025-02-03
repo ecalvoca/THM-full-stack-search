0a1c2db61034e494f7487f6c309a4710f42c9582
ecalvoca <elena.calvo.carazo@gmail.com>
2025-02-03 09:22:45 +0100

    merge components SearchInput and ClearBtn into SearchBar

--- packages/client/src/components/ClearBtn.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function ClearBtn({ onClearClick }) {
-    return (
-        <span className="left-pan" onClick={onClearClick}>
-            <i className="fa fa-close"></i>
-        </span>
-    );
-}
--- packages/client/src/components/SearchBar.tsx
+++ packages/client/src/components/SearchBar.tsx
@@ -1,13 +1,21 @@
-import SearchInput from "./SearchInput.tsx";
-import ClearBtn from "./ClearBtn.tsx";
 
 
 export default function SearchBar({ searchTerm, onSearchChange, showClearBtn, onClearClick }) {
     return (
         <div className="form">
             <i className="fa fa-search"></i>
-            <SearchInput value={searchTerm} onSearchChange={onSearchChange} />
-            {showClearBtn && <ClearBtn onClearClick={onClearClick} />}
+            <input
+                type="text"
+                value={searchTerm}
+                className="form-control form-input"
+                placeholder="Search accommodation..."
+                onChange={onSearchChange}
+            />
+            {showClearBtn &&
+                <span className="left-pan" onClick={onClearClick}>
+                    <i className="fa fa-close"></i>
+                </span>
+            }
         </div>
     );
 }
